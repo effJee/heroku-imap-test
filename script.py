0354b17ec04cdbbe895b226fd7e5d78e5d21f1b4
filip <sh333lby@gmail.com>
2019-05-15 10:04:04 +0200

    logging test

--- script.py
+++ script.py
@@ -1,5 +1,16 @@
+from os import environ  # for heroku
 import imapclient
+import rconfig  # for local tests
 
 print("Connecting...")
 imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 print("Connected")
+
+print("Logging in...")
+
+# heroku login
+# imapObj.login(environ.get('USERNAME_KEY'), environ.get('PASSWORD_KEY'))
+
+# local login
+imapObj.login(rconfig.address, rconfig.password)
+print("Logged in")
